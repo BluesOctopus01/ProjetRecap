fe565af65c689614e27838774a941875e987ba84
BluesOctopus01 <metens.thomas@outlook.be>
2026-05-01 12:41:24 +0200

    feat : implementation MetroAutomatique

--- MetroVille/Class/MetroAutomatique.cs
+++ MetroVille/Class/MetroAutomatique.cs
@@ -2,7 +2,24 @@ using System;
 
 namespace MetroVille.Class;
 
-public class MetroAutomatique
+public class MetroAutomatique : Vehicule
 {
+    private bool modeAutnome = true;
+    public MetroAutomatique(int numero, int capaciteMax, int puissanceMoteur) : base(numero, capaciteMax, puissanceMoteur)
+    {
+    }
 
+    public override void Demarrer()
+    {
+        Console.WriteLine($"Metro {Numero} - activation du pilote automatique");
+    }
+
+    public override string GetTypeVehicule()
+    {
+        return "Metro Automatique";
+    }
+    public void SignalerIncident(string description)
+    {
+        Console.WriteLine($"Incident Metro {Numero} : {description}");
+    }
 }
