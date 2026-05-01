fcf638b5408b84b949a8d45e868d58d376d7a614
BluesOctopus01 <metens.thomas@outlook.be>
2026-05-01 12:35:21 +0200

    feat : implementation Tram

--- MetroVille/Class/Tram.cs
+++ MetroVille/Class/Tram.cs
@@ -2,7 +2,22 @@ using System;
 
 namespace MetroVille.Class;
 
-public class Tram
+public class Tram : Vehicule
 {
+    private int numeroRail;
+    public Tram(int numero, int capaciteMax, int puissanceMoteur, int numeroRail) : base(numero, capaciteMax, puissanceMoteur)
+    {
+        this.numeroRail = numeroRail;
+    }
 
+    public override void Demarrer()
+    {
+        DemarrerMoteur();
+        Console.WriteLine($"Tram {Numero} sur rail {numeroRail} - prêt");
+    }
+
+    public override string GetTypeVehicule()
+    {
+        return "Tram";
+    }
 }
