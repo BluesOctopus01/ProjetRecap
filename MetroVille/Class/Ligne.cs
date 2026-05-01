79fd5afa537f252502dd1b3f5708879dcb767085
BluesOctopus01 <metens.thomas@outlook.be>
2026-05-01 13:16:05 +0200

    feat: implementation Ligne

--- MetroVille/Class/Ligne.cs
+++ MetroVille/Class/Ligne.cs
@@ -1,8 +1,36 @@
 using System;
+using System.IO.Compression;
 
 namespace MetroVille.Class;
 
 public class Ligne
 {
-
+    private int numeroLigne;
+    private Arret[] arrets = [];
+    private Vehicule[] vehicules = [];
+    public Ligne(int numeroLigne, Arret[] arrets,Vehicule[] vehicules)
+    {
+        this.numeroLigne = numeroLigne;
+        this.arrets = arrets;
+        this.vehicules = vehicules;
+    }
+    public void LancerService()
+    {
+        foreach ( Vehicule v in vehicules)
+        {
+            v.Demarrer();
+        }
+    }
+    public void SimulerTour()
+    {
+        foreach (Vehicule v in vehicules)
+        {
+            foreach(Arret a in arrets)
+            {
+                a.FaireMonter(5);
+                
+                v.AfficherEtat();
+            }
+        }
+    }
 }
