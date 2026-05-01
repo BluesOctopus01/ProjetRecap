112c2e35d3da678ec34f8ae2f496f80314b3aeac
BluesOctopus01 <metens.thomas@outlook.be>
2026-05-01 13:00:54 +0200

    feat: implémentation Arret

--- MetroVille/Class/Arret.cs
+++ MetroVille/Class/Arret.cs
@@ -4,5 +4,33 @@ namespace MetroVille.Class;
 
 public class Arret
 {
-
+    private int passagersEnAttente;
+    public string Nom { get; }
+    public Arret(string nom)
+    {
+        Nom = nom;
+        passagersEnAttente = 0;
+    }
+    public void AjouterPassagers(int nb)
+    {
+        if (nb < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(nb), "nb ne peut pas être négatif");
+        }
+        passagersEnAttente += nb;
+    }
+    public int FaireMonter(int nb)
+    {
+        if (nb < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(nb), "nb ne peut pas être négatif");
+        }
+        int nbQuiMontent = Math.Min(nb, passagersEnAttente);
+        passagersEnAttente -= nbQuiMontent;
+        return nbQuiMontent;
+    }
+    public int GetPassagersEnAttente()
+    {
+        return passagersEnAttente;
+    }
 }
